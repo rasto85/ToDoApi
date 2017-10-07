5ffaf1ee3db66eda6edfbe43680320a0e572635b
rasto85 <alexander.goude@hotmail.com>
2017-10-07 16:11:32 +0200

    Revert "3"
    This reverts commit 71228c1c8b99c7b8b00eb260043360f1bd46090e.

--- ToDoApi/Controllers/TodoController.cs
+++ ToDoApi/Controllers/TodoController.cs
@@ -23,7 +23,7 @@ namespace ToDoApi.Controllers
         {
 
             var todos = _context.ToDoItems;
-            var apa = 3;
+            var apa = 2;
             return Ok(todos);
         }
 
