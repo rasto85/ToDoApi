139ba976c4f74cb7cc251c0006c32811920a0fab
rasto85 <alexander.goude@hotmail.com>
2017-10-07 15:42:32 +0200

    First commit

---
+++ ToDoApi/Models/ToDoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApi.Models
+{
+    public class ToDoItem
+    {
+        public int ToDoItemId { get; set; }
+        public string Header { get; set; }
+        public bool IsImportant { get; set; }
+    }
+}
